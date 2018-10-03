0f1696492abedf04fe3d134d5438cf5247f9c134
pc\user <archbars@gmail.com>
2018-10-03 19:06:19 +0400

    v4

--- classes.py
+++ classes.py
@@ -8,6 +8,10 @@ class Animal:
     noise = ''
     male = 0  # 0-ж 1-м
     hungry = True
+    eggs_count = 0
+    last_get_milk = 0
+    milk_count = 0
+    last_shear = 0
 
     def __init__(self, reference, name, weight, male):
         self.reference = reference
@@ -24,18 +28,33 @@ class Animal:
     def do_feed(self):
         self.hungry = False
 
+    def collect_tax(self, tax_kind):
+        if tax_kind is 'eggs':
+            if (self.male == 0) and (self.eggs_count > 0):
+                cur_eggs_count = self.eggs_count
+                self.eggs_count = 0
+
+                return cur_eggs_count
+            else:
+                return 0
+        elif tax_kind is 'milk':
+            if self.last_get_milk > 12:
+                cur_milk_count = self.milk_count
+                self.last_get_milk = 0
+                self.milk_count = 0
+                return cur_milk_count
+
+            else:
+                return 0
+        elif tax_kind is 'wool':
+            if self.last_shear > 3:
+                print("Ура, постригли")
+            else:
+                print("Слишком рано, еще шерсть не отросла")
+
 
 class Birds(Animal):
     eggs_count = 1
-
-    def get_egg(self):
-        if (self.male == 0) and (self.eggs_count > 0):
-            cur_eggs_count = self.eggs_count
-            self.eggs_count = 0
-
-            return cur_eggs_count
-        else:
-            return 0
 
 
 class Livestock(Animal):  # класс заглушка, если потрбеуется добавление общих свойств для скота.
@@ -48,16 +67,6 @@ class Cows(Livestock):
     milk_count = 8
     last_get_milk = 13  # сколько часов назад последний раз доили
 
-    def get_milk(self):
-        if self.last_get_milk > 12:
-            cur_milk_count = self.milk_count
-            self.last_get_milk = 0
-            self.milk_count = 0
-            return cur_milk_count
-
-        else:
-            return 0
-
     def __init__(self, name, weight):
         self.name = name
         self.weight = weight
@@ -69,15 +78,6 @@ class Goats(Livestock):
     milk_count = 3
     last_get_milk = 13  # сколько часов назад последний раз доили
 
-    def get_milk(self):
-        if self.last_get_milk > 12:
-            cur_milk_count = self.milk_count
-            self.last_get_milk = 0
-            self.milk_count = 0
-            return cur_milk_count
-        else:
-            return 0
-
     def __init__(self, name, weight):
         self.name = name
         self.weight = weight
@@ -87,12 +87,6 @@ class Sheeps(Livestock):
     reference = 'Овца'
     noise = 'БЕЕЕ'
     last_shear = 1  # последний раз пострижена
-
-    def do_shear(self):
-        if self.last_shear > 3:
-            print("Ура, постригли")
-        else:
-            print("Слишком рано, еще шерсть не отросла")
 
     def __init__(self, name, weight):
         self.name = name
@@ -139,9 +133,9 @@ def lets_feed(animal_name):
 
 
 def lets_milk(animal_name):
-    # return animal_name.get_milk()
     if animal_name.last_get_milk > 12:
-        print('Доим {}, и получаем {} литров молока'.format(animal_name.name, animal_name.get_milk()))
+        print('Доим {}, и получаем {} литров молока'.format(animal_name.name, animal_name.collect_tax('milk')))
+
     else:
         print('{} доили недавно, пока нет молока'.format(animal_name.name))
 
@@ -155,8 +149,7 @@ def lets_egg(animal_name):
 
 def lets_shear(animal_name):
     print("Стрижем {}".format(animal_name.name))
-    animal_name.do_shear()
-    print()
+    animal_name.collect_tax('wool')
 
 
 goose1 = Gooses('Серый', '18', 'Серый', 1)
@@ -170,51 +163,28 @@ goat2 = Goats("Копыта", 32)
 sheep1 = Sheeps("Барашек", 20)
 sheep2 = Sheeps("Кудрявый", 20)
 
-print('Корм животных:')
+
+all_animals = [goose1, goose2, chicken1, chicken2, duck1, cow, goat1, goat2, sheep1, sheep2]
+
 sheep2.hungry = False
 cow.hungry = False
-
-lets_feed(goose1)
-lets_feed(goose2)
-lets_feed(chicken1)
-lets_feed(chicken2)
-lets_feed(duck1)
-lets_feed(cow)
-lets_feed(goat1)
-lets_feed(goat2)
-lets_feed(sheep1)
-lets_feed(sheep2)
-
-
-print()
-print("Дойка:")
-# print('Доим {}, и получаем {} литров молока'.format(cow.name, cow.get_milk()))
-# print('Доим {}, и получаем {} литров молока'.format(goat1.name, goat1.get_milk()))
 goat2.last_get_milk = 2
-# print('Доим {}, и получаем {} литров молока'.format(goat2.name, goat2.get_milk()))
-
-lets_milk(cow)
-lets_milk(goat1)
-lets_milk(goat2)
-
-lets_egg(goose1)
-lets_egg(goose2)
-lets_egg(chicken1)
-lets_egg(chicken2)
-lets_egg(duck1)
-print()
-print("Стрижка:")
 sheep2.last_shear = 5
-lets_shear(sheep2)
-lets_shear(sheep1)
-
+for i in all_animals:
+    lets_feed(i)# кормим
+    if i.last_get_milk > 0:
+        lets_milk(i)# доим
+    if i.__class__.mro().__contains__(Birds):
+        lets_egg(i)# собираем яйцы
+    if i.__class__.mro().__contains__(Sheeps):
+        lets_shear(i)# стрижем
+print()
 
 weight_sum = 0
 heavy = 0
 heavy_name = ''
 for obj in gc.get_objects():
     if isinstance(obj, Animal):
-        # print(obj.__dict__)
         weight_sum += int(obj.__dict__['weight'])
         if int(obj.__dict__['weight']) > heavy:
             heavy = int(obj.__dict__['weight'])
